bbc05dc0ad7ca108952c10bf77e049106fae0a4f
chen-yw <chen-yw23@mails.tsinghua.edu.cn>
2024-06-20 19:48:08 +0800

    something new

--- try_tabnine.py
+++ try_tabnine.py
@@ -1,4 +1,8 @@
 import numpy as np  
 import pandas as pd
 import akshare as ak
+print('numpy version:', np.__version__)
+print('pandas version:', pd.__version__)
 print('akshare version:', ak.__version__)
+
+print("hello world! This is an AI code execution environment.")
